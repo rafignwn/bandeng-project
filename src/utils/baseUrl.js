bf1c88991ba66ee450bf72b9d05c1ca6c2e3a9cb
rafignwn <figun746@gmail.com>
2022-06-06 00:39:16 +0700

    update base url

--- src/utils/baseUrl.js
+++ src/utils/baseUrl.js
@@ -1,2 +1,3 @@
-export const BASE_URL = "https://bandeng-serve.somee.com/api";
+export const BASE_URL = "http://www.bandeng-serve.somee.com";
+// export const BASE_URL = "https://bandeng-serve.somee.com/api";
 // export const BASE_URL = "http://localhost:5000/api";
