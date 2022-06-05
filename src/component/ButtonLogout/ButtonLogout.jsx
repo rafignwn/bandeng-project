20225e3a3bd3c7b4bc0f4446950d706eb13bf162
rafignwn <figun746@gmail.com>
2022-06-06 01:22:00 +0700

    repair bug logout

--- src/component/ButtonLogout/ButtonLogout.jsx
+++ src/component/ButtonLogout/ButtonLogout.jsx
@@ -2,20 +2,18 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import "./buttonLogoutStyles.css";
+import { BASE_URL } from "../../utils/baseUrl";
 
 export default function ButtonLogout() {
   const navigate = useNavigate();
   const logoutHandler = async () => {
     try {
-      const response = await axios.get(
-        "http://localhost:5000/api/auth/logout",
-        {
-          withCredentials: true,
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      );
+      const response = await axios.get(`${BASE_URL}/auth/logout`, {
+        withCredentials: true,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
       console.log(response.data);
       navigate("/");
     } catch (error) {
